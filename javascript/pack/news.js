8d36028d2b5ca8fa03067996dcde1cd9f69e51a1
Jose Matias <josematias92@gmail.com>
2021-04-21 22:34:23 -0500

    added the links for those particular articles

--- javascript/pack/news.js
+++ javascript/pack/news.js
@@ -24,6 +24,13 @@ let articleContent5 = document.querySelector('#content-article-5')
 let articleTitle6 = document.querySelector('#title-article-6')
 let articleContent6 = document.querySelector('#content-article-6')
 
+let linkNYT0 = document.querySelector('#link-to-report-0')
+let linkNYT1 = document.querySelector('#link-to-report-1')
+let linkNYT2 = document.querySelector('#link-to-report-2')
+let linkNYT3 = document.querySelector('#link-to-report-3')
+let linkNYT4 = document.querySelector('#link-to-report-4')
+let linkNYT5 = document.querySelector('#link-to-report-5')
+
 
 fetch("https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=GZXll423rODlAPqfOTa4nNHwyXtQs31K", requestOptions)
     .then(response =>
@@ -34,19 +41,36 @@ fetch("https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=GZXll42
 
     articleTitle1.innerText = newsData.results[randomNumber].section
     articleContent1.innerText = newsData.results[randomNumber].abstract
+    linkNYT0.href = newsData.results[randomNumber].url
 
     articleTitle2.innerText = newsData.results[1].section
     articleContent2.innerText = newsData.results[1].abstract
+    linkNYT1.href = newsData.results[1].url
 
     articleTitle3.innerText = newsData.results[2].section
     articleContent3.innerText = newsData.results[2].abstract
+    linkNYT2.href = newsData.results[2].url
 
     articleTitle4.innerText = newsData.results[3].section
     articleContent4.innerText = newsData.results[3].abstract
+    linkNYT3.href = newsData.results[3].url
 
     articleTitle5.innerText = newsData.results[4].section
     articleContent5.innerText = newsData.results[4].abstract
+    linkNYT4.href = newsData.results[4].url
 
     articleTitle6.innerText = newsData.results[5].section
     articleContent6.innerText = newsData.results[5].abstract
+    linkNYT5.href = newsData.results[5].url
 })
+
+fetch("https://swapi.dev/api/people/")
+    .then(response =>
+    response.text())
+    .then(result => {
+    console.log(JSON.parse(result), 'result')
+    starWarsData = JSON.parse(result)
+    
+    console.log(starWarsData.results[0])
+
+    })
