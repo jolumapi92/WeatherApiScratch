91829fffa36ee43e01d5081a2d91a6260c755a96
Jose Matias <josematias92@gmail.com>
2021-04-26 14:36:32 -0500

    addee

--- javascript/pack/star_wars.js
+++ javascript/pack/star_wars.js
@@ -1,10 +1,30 @@
 fetch("https://swapi.dev/api/people/")
-    .then(response =>
-    response.text())
-    .then(result => {
-    console.log(JSON.parse(result), 'result')
-    starWarsData = JSON.parse(result)
-    
-    console.log(starWarsData.results[0])
+.then(response =>
+response.text())
+.then(result => {
+console.log(JSON.parse(result), 'result')
+starWarsData = JSON.parse(result)
+console.log(starWarsData.results[0])
+});
 
-    })
+
+
+let selector = document.querySelector('#collection-characters');
+let nameC = document.querySelector('#name')
+let birth = document.querySelector('#birth')
+let eye = document.querySelector('#eye')
+let gender = document.querySelector('#gender')
+let height = document.querySelector('#height')
+
+selector.addEventListener('change', function(event){
+    selected = event.target.value;
+
+    nameC.innerText = `Name: ${starWarsData.results[selected]["name"]}`
+    birth.innerText = `Birth Year: ${starWarsData.results[selected]["birth_year"]}`
+    eye.innerText = `Eye Color: ${starWarsData.results[selected]["eye_color"]}`
+    gender.innerText = `Gender: ${starWarsData.results[selected]["gender"]}`
+    height.innerText = `Height: ${starWarsData.results[selected]["height"]}`
+})
+
+
+
