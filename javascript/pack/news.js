27b1ad9045709f7dd8b0605fbcdcb1f17c0d02ae
Jose Matias <josematias92@gmail.com>
2021-04-26 19:19:08 -0500

    addded

--- javascript/pack/news.js
+++ javascript/pack/news.js
@@ -31,6 +31,51 @@ let linkNYT3 = document.querySelector('#link-to-report-3')
 let linkNYT4 = document.querySelector('#link-to-report-4')
 let linkNYT5 = document.querySelector('#link-to-report-5')
 
+let headerPage = document.querySelector('#header-page')
+
+let querySpace = document.querySelector('#input-query');
+
+querySpace.addEventListener('change', function(event){
+    console.log(event.target.value)
+    queryContent = event.target.value
+
+    headerPage.innerText = `You looked for ${queryContent}`
+
+    fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${queryContent}&api-key=GZXll423rODlAPqfOTa4nNHwyXtQs31K`, requestOptions)
+    .then(response =>
+    response.text())
+    .then(result => {
+    console.log(JSON.parse(result), 'result')
+    queryData = JSON.parse(result)
+
+    
+
+    articleTitle1.innerText = queryData.response.docs[0].section_name
+    articleContent1.innerText = queryData.response.docs[0].abstract
+    linkNYT0.href = queryData.response.docs[0].web_url
+
+    articleTitle2.innerText = queryData.response.docs[1].section_name
+    articleContent2.innerText = queryData.response.docs[1].abstract
+    linkNYT1.href = queryData.response.docs[1].web_url
+
+    articleTitle3.innerText = queryData.response.docs[2].section_name
+    articleContent3.innerText = queryData.response.docs[2].abstract
+    linkNYT2.href = queryData.response.docs[2].web_url
+
+    articleTitle4.innerText = queryData.response.docs[3].section_name
+    articleContent4.innerText = queryData.response.docs[3].abstract
+    linkNYT3.href = queryData.response.docs[3].web_url
+
+    articleTitle5.innerText = queryData.response.docs[4].section_name
+    articleContent5.innerText = queryData.response.docs[4].abstract
+    linkNYT4.href = queryData.response.docs[4].web_url
+
+    articleTitle6.innerText = queryData.response.docs[5].section_name
+    articleContent6.innerText = queryData.response.docs[5].abstract
+    linkNYT5.href = queryData.response.docs[5].web_url
+    })
+})
+
 
 fetch("https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=GZXll423rODlAPqfOTa4nNHwyXtQs31K", requestOptions)
     .then(response =>
